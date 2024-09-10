eef79e7cbeb8d93910446a71cdb98c9f648ddc66
Alexandra Edwards <alexed93@gmail.com>
2024-09-10 14:38:05 +0100

    Set up session data feedback and _BASE draft

--- app/routes.js
+++ app/routes.js
@@ -9,4 +9,30 @@ const router = govukPrototypeKit.requests.setupRouter()
 const radioButtonRedirect = require('radio-button-redirect')
 router.use(radioButtonRedirect)
 
+// Useful things
+
+////// Vicky Teinaki 
+////// https://www.vickyteinaki.com/blog/more-efficient-prototyping-with-the-gov-uk-prototype-kit-step-by-step/
+
+// Logging session data  
+router.use((req, res, next) => {    
+    const log = {  
+      method: req.method,  
+      url: req.originalUrl,  
+      data: req.session.data  
+    }  
+    console.log(JSON.stringify(log, null, 2))  
+   
+  next()  
+}) 
+
+router.use('/', (req, res, next) => {  
+    res.locals.currentURL = req.originalUrl; //current screen  
+    res.locals.prevURL = req.get('Referrer'); // previous screen
+
+    console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
+
+    next();  
+});
+
 // Add your routes here
