cfbcc0fb32327fd9006f63b4e2cd78b8a3c656b7
Alexandra Edwards <alexed93@gmail.com>
2025-05-07 11:04:27 +0100

    Add notification banner to share link

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,13 +4,23 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
-
   // Share button for reporting pages
   const copyLinkURL = document.getElementById('js-copy-link-btn');
-  copyLinkURL.addEventListener('click', () => {
-      // Copy the link
+  const copyLinkBanner = document.getElementById('js-copy-link-banner');
+
+  if (copyLinkURL && copyLinkBanner) {
+    copyLinkURL.addEventListener('click', () => {
       const link = window.location.href;
-      navigator.clipboard.writeText(link);
-  });
-})
+
+      navigator.clipboard.writeText(link).then(() => {
+        // Show banner if copy succeeds
+        copyLinkBanner.classList.remove('govuk-!-display-none');
+        copyLinkBanner.classList.add('govuk-!-display-block');
+      }).catch(err => {
+        console.error('Failed to copy the link: ', err);
+        // Optionally show an error message to the user here
+      });
+    });
+  }
+  
+});
